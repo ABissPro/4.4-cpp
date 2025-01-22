94bc539d7eaa488f41d046e0a2ed1f4c549adc29
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-22 22:56:09 +0300

    Add files via upload

---
+++ Калькулятор опыта.cpp
@@ -0,0 +1,32 @@
+﻿// Калькулятор опыта.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int exPoints;
+    int level = 1;
+
+    std::cout << "Введите число очков опыта: ";
+    std::cin >> exPoints;
+    std::cout << "---Считаем---\n";
+
+    if (exPoints < 0) {
+        std::cout << "Данные введены некорректно!";
+    }
+    else if (exPoints >= 1000 && exPoints < 2500) {
+        level = 2;
+        std::cout << "Ваш уровень: " << level << "\n";
+    }
+    else if (exPoints >= 2500 && exPoints < 5000) {
+        level = 3;
+        std::cout << "Ваш уровень: " << level << "\n";
+    }
+    else if (exPoints >= 5000) {
+        level = 4;
+        std::cout << "Ваш уровень: " << level << "\n";
+    }
+
+}
+
