c37a152b7256ebe54743532751784b72947a1ed0
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-01-23 14:38:01 +0300

    Add files via upload

---
+++ Прогрессивный налог.cpp
@@ -0,0 +1,34 @@
+﻿// Прогрессивный налог.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int salary;
+    float tax;
+
+    std::cout << "Введите доход: \n";
+    std::cin >> salary;
+
+    if (salary < 0) {
+        std::cout << "Введены некорректные данные!\n";
+    }
+    else if (salary == 0) {
+        std::cout << "Грустно :(\n";
+    }
+    else if (salary <= 10000) {
+        tax = salary * 0.13;
+        std::cout << "С дохода " << salary << " надо заплатить такой налог: " << tax << "\n";
+    }
+    else if ((salary > 10000) && (salary <= 50000)) {
+        tax = 0.2 * (salary - 10000) + 0.13 * 10000;
+        std::cout << "С дохода " << salary << " надо заплатить такой налог: " << tax << "\n";
+    }
+    else if ((salary > 50000) && (salary <= 100000)) {
+        tax = 0.3 * (salary - 50000) + 0.2 * ((salary - 50000) - 10000) + 0.13 * 10000;
+        std::cout << "С дохода " << salary << " надо заплатить такой налог: " << tax << "\n";
+    }
+}
+
+
